8be51c63a092976783bcbf180b7187c1ff27d6ab
Vitor Gabriel Vido <vitorlatero03@gmail.com>
2026-03-05 18:23:05 -0300

    Index Funcionando

--- projeto_sala_de_aula_ronan/app.py
+++ projeto_sala_de_aula_ronan/app.py
@@ -1,14 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template  # Import correto
 
 app = Flask(__name__)
 
+# Rota raiz: mantém a mensagem de texto
 @app.route("/")
 def home():
-    return "Servidor Flask funcionando 100% 🚀"
+    return "Servidor funcionando 🚀"
+
+# Nova rota para index.html
+@app.route("/index")  # ✅ precisa começar com barra
+def show_index():
+    return render_template('index.html')  # chama o HTML
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-
-
